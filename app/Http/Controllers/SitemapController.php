42eff7889aef27a816af10b7443b84aa8b4320bc
Richard Le Poidevin <ric@wearebwi.com>
2022-11-07 13:52:52 +0000

    XML sitemap

--- app/Http/Controllers/SitemapController.php
+++ app/Http/Controllers/SitemapController.php
@@ -17,80 +17,17 @@ namespace App\Http\Controllers;
 
 
 use App\Storyblok\Folders\Episodes;
-use Illuminate\Support\Str;
 
 class SitemapController extends Controller
 {
 
 	public function index() {
-		$storyblokClient = resolve('Storyblok\Client');
-
-		// single item
-		// staff uuid: 428f6684-53b1-4503-81b6-c9f9702d2a87   - key_contact
-		// office uuid: 8b9b0820-10cc-4b4d-9132-06228ac3efc5  - contact_point
-		// faq uuid: af049d5a-056c-4fc5-99e6-0078353c4c15  - faqs
-
-		// multi item
-		// product uuid: 04bb8cb2-f740-4ec5-a943-a62d6cc417fe
-
-		/*$stories = $storyblokClient->getStories([
-			"content_type" => "simple-product",
-			"filter_query" =>  [
-				"title" =>  [
-					"in" => "Pensions for International Clients"
-				]
-			]
-		]);*/
-
-
-/*
-		// single item
-		$stories = $storyblokClient->getStories([
-			"content_type" => "product",
-			"filter_query" =>  [
-				"contact_point" =>  [ // relationship name
-					"is" => "8b9b0820-10cc-4b4d-9132-06228ac3efc5" // known page
-				]
-			]
-		]);
-*/
-
-		// multiple items
-		$stories = $storyblokClient->getStories([
-			"content_type" => "product",
-			"filter_query" =>  [
-				"faqs" =>  [ // relationship name
-					"exists" => "af049d5a-056c-4fc5-99e6-0078353c4c15" // known page
-				]
-			]
-		]);
-
-
-
-
-		dd($stories->getBody());
-
-		$links = collect($storyblokClient->getLinks()->responseBody['links'])->filter(function($item) {
-			//return !Str::contains($item['slug'], 'people');
-			return Str::contains($item['slug'], 'staff');
-		})->filter(function($item) {
-			return !Str::contains($item['slug'], 'updates-and-news');
-		})->take(10)->transform(function ($item, $uuid) use ($storyblokClient) {
-			$story = $storyblokClient->resolveRelations('team.people')->getStoryByUuid($uuid);
-			dd($item, $story);
-			$item['links'] = $story;
-			return $item;
-		})->dd();
-
-
-		/*
 		$folder = new Episodes();
 		$folder->settings(['per_page' => 50]);
 		$folder->slug('episodes');
 
 		return view('sitemap', [
 			'episodes' => $folder->read(),
-		]);*/
-
+		]);
 	}
 }
